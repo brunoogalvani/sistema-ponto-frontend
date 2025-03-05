64dacb090a0536aa0b438d626a9617cb1e8a1f3c
brunoogalvani <bthezolin@gmail.com>
2025-03-04 23:34:45 -0300

    Página de Registro e nome de usuário no Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,31 @@
+import { useState, useEffect } from 'react';
 import './Home.css'
 import api from '../../services/api'
 
 function Home() {
 
+  const [userName, setUserName] = useState("");
   const userId = sessionStorage.getItem('userId');
-  console.log(userId);
 
-  async function baterPonto(id) {
+  useEffect(() => {
+    async function getUserName() {
+      try {
+        const response = await api.get(`/users/${userId}`);
+        setUserName(response.data.name)
+      } catch (error) {
+        console.error("Erro ao buscar o usuário:", error);
+      }
+    }
+
+      if (userId) {
+        getUserName();
+      }
+
+  }, [userId]);
+
+  async function baterPonto() {
     try {
-      await api.post(`/pontos/bater/${id}`)
+      await api.post(`/pontos/bater/${userId}`)
     } catch (error) {
       alert("Todas as marcações de ponto do dia já foram preenchidas")
       console.error("Erro ao registrar ponto:", error);
@@ -19,7 +36,8 @@ function Home() {
     <>
       <div className="home-container">
         <h1>Registro de Ponto</h1>
-        <button type="button" onClick={() => baterPonto(userId)}>Registrar Ponto</button>
+        <h3>Bem-vindo, {userName}</h3>
+        <button type="button" onClick={baterPonto}>Registrar Ponto</button>
       </div>
     </>
   )
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,11 +1,44 @@
+import { useRef } from 'react'
+import { useNavigate } from 'react-router-dom';
 import './Register.css'
 import api from '../../services/api'
 
 function Register() {
 
+    const navigate = useNavigate();
+    const inputName = useRef();
+    const inputLogin = useRef();
+    const inputPassword = useRef();
+
+    async function cadastrar() {
+        try {
+            const response = await api.post('/users/register', {
+                name: inputName.current.value,
+                login: inputLogin.current.value,
+                password: inputPassword.current.value,
+                role: 1
+            })
+
+            if (response.status === 200) {
+                navigate('/');
+            }
+        } catch (error) {
+            console.error("Erro no cadastro", error);
+            alert("Usuário já existe");
+        }
+    }
+
     return (
         <>
-            <h1>Hello World!</h1>
+            <div className='register-container'>
+                <form>
+                    <h1>Cadastre-se</h1>
+                    <input placeholder='Nome' name='name' type='text' ref={inputName} autoComplete='off' />
+                    <input placeholder='Login' name='login' type='text' ref={inputLogin} autoComplete='off' />
+                    <input placeholder='Senha' name='senha' type='password' ref={inputPassword} autoComplete='off' />
+                    <button type='button' onClick={cadastrar}>Cadastrar</button>
+                </form>
+            </div>
         </>
     )
 }
